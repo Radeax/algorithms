b87bacfd85a33fe39764be6a774ccc47f8f86adf
Radeax <joflip@gmail.com>
2020-01-27 14:53:04 -0500

    Bonus 2: return a string (instead of mulitple logs)

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -20,6 +20,10 @@ Example - fizzbuzz(15) would return the following string:
 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, and FizzBuzz. */
 
 function FizzBuzz(n) {
+  let str = '';
+  let a = 'Fizz';
+  let b = 'Buzz';
+
   // Check if positive number
   if (n < 0 || typeof n != 'number')
     return null;
@@ -27,18 +31,31 @@ function FizzBuzz(n) {
   // Check if divisible by 3 AND 5, 3, or 5
   for (let i = 1; i <= n; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
+      str += a + b;
     }
     else if (i % 3 == 0) {
-      console.log("Fizz");
+      str += a;
     }
     else if (i % 5 == 0) {
-      console.log("Buzz");
+      str += b;
     }
     else {
-      console.log(i);
+      str += i;
+    }
+
+    // Add punctuation between numbers
+    if (i + 1 < n) {
+      str += ', ';
+    }
+    else if (i == n) {
+      str += '.';
+    }
+    else {
+      str += ', and '
     }
   }
+
+  console.log(str);
 }
 
 FizzBuzz(15);
