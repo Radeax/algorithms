86bd88a9c48cbb6caa54e7bda865311d7308aac6
Radeax <joflip@gmail.com>
2020-04-25 20:36:59 -0400

    Array.js - Rotate

--- Arrays.js
+++ Arrays.js
@@ -109,6 +109,36 @@ function reverse(arr) {
     return arr;
 }
 
+// -- Rotate -- //
+// Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr's values to the right
+// by that amount. 'Wrap-around' any values that shift off array's end to the other side, so that no data is
+// lost. Operate in-place: given ([1, 2, 3], 1), change the array to [3, 1, 2]. Don't use built-in functions.
+// Second: allow negative shiftBy (shift L, not R).
+// Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+// Fourth: minimize the touches of each element.
+function rotateArr(arr, shiftBy) {
+    const arrayLength = arr.length;
+    shiftBy %= arrayLength;
+    if (shiftBy >= 0) {
+        for (let i = 0; i < shiftBy; i++) {
+            const temp = arr[arrayLength - 1];
+            for (let i = arrayLength - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = temp;
+        }
+    }
+    else {
+        for (let i = 0; i > shiftBy; i--) {
+            const temp = arr[0];
+            for (let i = 0; i < arrayLength - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arrayLength - 1] = temp;
+        }
+    }
+}
+
 const array = [3, 27, -15, 13, 13, 8, 8, 5];
 let val = 10;
 
@@ -120,5 +150,6 @@ console.log(array);
 // swapPairs(array);
 // removeDuplicates(array);
 // minToFront(array);
-reverse(array);
+// reverse(array);
+rotateArr(array, -12366233444);
 console.log(array);
